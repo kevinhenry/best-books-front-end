98e183b17b0846f587a6c230fa12d750e572e5d5
Qadree Trimble <qadree_t@yahoo.com>
2021-04-24 18:10:53 -0700

    added a card comppnent for styling

--- src/Profile.js
+++ src/Profile.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -11,9 +13,19 @@ const Profile = () => {
   return (
     isAuthenticated && (
       <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
+        <Container>
+          <Card style={{ width: '15rem' }}>
+            <Card.Img variant="top" style={{ width: '15rem' }} src={user.picture} />
+            <Card.Body>
+              <Card.Title>{user.name}'s Profile</Card.Title>
+              <Card.Text>
+                <p>{user.email}</p>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Container>
+        {/* <h2>{user.name}</h2>
+        <p>{user.email}</p> */}
       </div>
     )
   );
